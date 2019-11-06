42c0d7fc6bef7263d456054e59242507697ad48e
berkinsili@gmail.com <berkinsili@Berkins-MacBook-Pro.local>
2019-11-06 20:30:44 +0300

    Complex Shape Added

--- PathsNLayers/DemoView.swift
+++ PathsNLayers/DemoView.swift
@@ -18,7 +18,8 @@ class DemoView: UIView {
         self.backgroundColor = UIColor.darkGray
 //        simpleShapeLayer()
 //        shapeLayerAsMask()
-        twoShapes()
+//        twoShapes()
+        complexShape()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -35,6 +36,31 @@ class DemoView: UIView {
 //        // Specify a border (stroke) color.
 //        UIColor.purple.setStroke()
 //        path.stroke()
+    }
+    
+    func complexShape() {
+        path = UIBezierPath()
+        path.move(to: CGPoint(x: 0.0, y: 0.0))
+        path.addLine(to: CGPoint(x: self.frame.size.width/2 - 50.0, y: 0.0))
+        path.addArc(withCenter: CGPoint(x: self.frame.size.width/2 - 25.0, y: 25.0),
+                    radius: 25.0,
+                    startAngle: CGFloat(180.0).toRadians(),
+                    endAngle: CGFloat(0.0).toRadians(),
+                    clockwise: false)
+        path.addLine(to: CGPoint(x: self.frame.size.width/2, y: 0.0))
+        path.addLine(to: CGPoint(x: self.frame.size.width - 50.0, y: 0.0))
+        path.addCurve(to: CGPoint(x: self.frame.size.width, y: 50.0),
+                      controlPoint1: CGPoint(x: self.frame.size.width + 50.0, y: 25.0),
+                      controlPoint2: CGPoint(x: self.frame.size.width - 150.0, y: 50.0))
+        path.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height))
+        path.addLine(to: CGPoint(x: 0.0, y: self.frame.size.height))
+        path.close()
+     
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+     
+        self.backgroundColor = UIColor.orange
+        self.layer.mask = shapeLayer
     }
     
     func twoShapes() {
